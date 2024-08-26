5e8c0f3e88b5890d5827d001f2d62fb2ca7d9f20
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-26 22:45:04 +0800

    added theme and modified code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:crud_activity/screen/tab_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:crud_activity/student.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Student App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Student(), // Use the Student screen
+      theme: theme,
+      home: const TabScreen(), // Use the Student screen
     );
   }
 }
